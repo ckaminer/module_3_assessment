5f007dbaedb6b851760e04bc7985e6f69d7b964c
Charles Kaminer <ckamine@g.clemson.edu>
2016-08-02 09:32:13 -0600

    item show test and implemented test for item show method implemented and tests passing

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::ItemsController < ApiBaseController
   def index
     render json: Item.all, each_serializer: ItemSerializer
   end
+
+  def show
+    render json: Item.find(params[:id]), serializer: ItemSerializer
+  end
+
+  def destroy
+    render json: Item.find(params[:id]).destroy
+    render status: 204
+  end
 end
--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -9,12 +9,38 @@ RSpec.describe Api::V1::ItemsController do
       expect(response.status).to eq 200
 
       items = JSON.parse(response.body)
-      expect(items.count).to eq 1
-      expect(items.first["name"]).to eq "First Item"
-      expect(items.first["description"]).to eq "First Description"
-      expect(items.first["image_url"]).to eq "First image url"
+      expect(items.count).to eq 2
+      expect(items.first["name"]).to eq "Second Item"
+      expect(items.first["description"]).to eq "Second Description"
+      expect(items.first["image_url"]).to eq "Second image url"
       expect(items.first["created_at"]).to eq nil
       expect(items.first["updated_at"]).to eq nil
+    end
+  end
+
+  describe "GET show" do
+    it "can get single specified items" do
+      item = items(:one)
+      get :show, id: item.id
+
+      expect(response.status).to eq 200
+
+      item = JSON.parse(response.body)
+      expect(item["name"]).to eq "First Item"
+      expect(item["description"]).to eq "First Description"
+      expect(item["image_url"]).to eq "First image url"
+      expect(item["created_at"]).to eq nil
+      expect(item["updated_at"]).to eq nil
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "can get single specified items" do
+      item = items(:one)
+      delete :destroy, id: item.id
+
+      expect(response.status).to eq 204
+      expect(items.count).to eq 1
     end
   end
 end
