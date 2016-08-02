777cfda02af65c62615348bb5af5638823a67394
Charles Kaminer <ckamine@g.clemson.edu>
2016-08-02 11:17:23 -0600

    create cassette for feature spec tidy up information transitions start working on views

--- app/models/store_search.rb
+++ app/models/store_search.rb
@@ -1,6 +1,6 @@
 class StoreSearch
   attr_reader :zip
-  
+
   def initialize(zip)
     @zip = zip
   end
@@ -10,10 +10,16 @@ class StoreSearch
   end
 
   def get_stores
-    service.get_stores(zip)
+    stores = raw_store_responses["stores"].map do |store|
+      Store.new(store)
+    end
   end
 
   private
+  def raw_store_responses
+    service.get_stores(zip)
+  end
+
   def service
     @service ||= SearchService.new
   end
